3d920d1f92ed344abdf973ac64149521b7219abd
Steven Smith <github@icodeforyou.com>
2020-01-18 17:22:11 -0500

    baseline

--- lib/flutter_shake_vibrate_package.dart
+++ lib/flutter_shake_vibrate_package.dart
@@ -1,7 +1,3 @@
 library flutter_shake_vibrate_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/on_shake_handler.dart';
